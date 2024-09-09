8486aed38658da9df6be82b93a85ff53a202c86f
ensiyehe <ensiyeh.esfandiari@gmail.com>
2024-09-09 18:15:48 +0200

    initial commit

---
+++ starter/src/js/config.js
@@ -0,0 +1,13 @@
+ 
+ // this is a constant that will never change 
+ export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes/';
+
+ export const TIMEOUT_SEC = 10 ;
+
+  export const RES_PER_PAGE = 10 ;
+
+  export const KEY = 'fa420c71-3b68-4a16-9017-c216197e2166'
+ 
+  export const MODAL_CLOSE_SEC = 2.5 ;
+
+ 
